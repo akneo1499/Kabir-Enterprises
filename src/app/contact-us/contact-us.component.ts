fb9a37e40732ec958d7c8177ffdcaabb0fcfc75e
aafaque <akneo1499@gmail.com>
2021-09-11 15:26:47 +0530

    Contact page functionality

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormControl, Validators } from "@angular/forms";
+import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+import emailjs from 'emailjs-com';
 
 @Component({
   selector: "app-contact-us",
@@ -9,18 +10,39 @@ import { FormBuilder, FormControl, Validators } from "@angular/forms";
 export class ContactUsComponent implements OnInit {
   FormData;
 
-  constructor(private builder: FormBuilder) {}
+  constructor() {}
 
   ngOnInit() {
-    this.FormData = this.builder.group({
-      Fullname: new FormControl("", [Validators.required]),
-      Email: new FormControl("", [Validators.compose([Validators.required, Validators.email])]),
-      Subject: new FormControl("", [Validators.required]),
-      Message: new FormControl("", [Validators.required]),
+    this.FormData = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z \-\']+')]),
+      email: new FormControl('', [Validators.compose([Validators.required, Validators.email])]),
+      phone: new FormControl('', [Validators.required, Validators.pattern("[- +()0-9]{10}")]),
+      message: new FormControl('', [Validators.required]),
     });
   }
 
-  onSubmit(event){
-    console.log(event);
+  public sendEmail(event) {
+    const serviceID = 'service_9g9kqie';
+    const templateID = 'template_t21wn6j';
+    const userID = 'user_sfneCMb69WYyr36OTf92K'
+    emailjs.send(serviceID, templateID, event, userID)
+    .then((response) => {
+      console.log('SUCCESS!', response.status, response.text);
+      alert("Message sent successfully.");
+      this.FormData.reset();
+   }, (err) => {
+      console.log('FAILED...', err);
+      alert("Failed to send the message.")
+   });
   }
+
+
+
+  // const templateParams = {
+  //     name: 'James',
+  //     notes: 'Check this out!'
+  // };
+  
+
 }
+
